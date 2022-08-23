034341957adda99d55d11585d15d4db2b5fc34e1
reinhard <reinhard2057@gmail.com>
2022-08-23 19:07:48 +0000

    thus

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,7 +12,10 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list->next;
 
-	while (fast != NULL)
+	if (!list)
+		return (0);
+
+	while ((fast != NULL) && slow)
 	{
 		if (slow == fast)
 		{
