4219276a3a725800bfe0f7383da327f86d88a231
reinhard <reinhard2057@gmail.com>
2022-08-29 16:12:09 -0700

    this

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,42 +1,84 @@
 #include "lists.h"
 /**
- * is_palindrome - function in C that checks if a
- *  singly linked list is a palindrome.
- *  @head: pointer to start of linked list
- *  Return: 0 if it is not a palindrome, 1 if it is a palindrome
- *  Description: if head pointer is equal to null return 1
- *  set flag to 1 and assigned two pointers right and left
- *  to point to the start and end of the list and compare
- *  intigers at those nodes if the intigers are equal the left
- *  pointer is moved forward and the right is moved backward kind of
- *  with the use of a marker mk.. just read the code...
+ * is_palindrome - checks if a list is palindrome
+ * @head: pointer to the head of the list
+ * Return: 1 if is and 0 if not
  */
+
 int is_palindrome(listint_t **head)
 {
-	listint_t *lt = *head;
-	listint_t *rt = *head;
-	listint_t *mkr = NULL;
+	listint_t *slow = *head, *fast = *head, *mid = NULL;
+	listint_t *temp = *head;
+	int flag;
+
+	while (1)
+	{
+		fast = fast->next->next;
+		if (!fast)
+		{
+			slow = slow->next;
+			mid = slow->next;
+			break;
+		}
+
+		if (!fast->next)
+		{
+			slow = slow->next->next;
+			mid = slow;
+			break;
+		}
+	}
+
+	reverse(&mid);
+	flag = check(&temp, &mid);
+	reverse(&mid);
+
+	return (flag);
+}
+
+/**
+ * reverse - reverse a linked list
+ * @head: pointer to the middle of the original list
+ * Return: none
+ */
+void reverse(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL, *temp = *head;
+
+	while (temp)
+	{
+		next = temp->next;
+		temp->next = prev;
+		prev = temp;
+		temp = next;
+	}
+	*head = prev;
+}
+
+/**
+ * check - actual check for the two halves two halves of the list
+ * @head1: pointer to the head of the original list
+ * @mid1: pointer to the pointer to the head of the reversed list
+ *
+ * Return: intiger 1 or 0
+ */
+int check(listint_t **head1, listint_t **mid1)
+{
+	listint_t *head = *head1, *mid = *mid1;
 	int flag = 1;
 
-	if (*head == NULL)
-		return (1);
-
-	while (flag == 1)
+	while (head && mid)
 	{
-		rt = *head;
-		while (rt->next != mkr)
-			rt = rt->next;
-		mkr = rt;
-
-		if (lt->n != rt->n)
+		if (head->n == mid->n)
+		{
+			head = head->next;
+			mid = mid->next;
+		}
+		else
 		{
 			flag = 0;
 			break;
 		}
-		if (lt->next == rt || lt == rt)
-			break;
-
-		lt = lt->next;
 	}
 	return (flag);
 }
---
+++ 0x03-python-data_structures/test_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+/**
+ * is_palindrome - function in C that checks if a
+ *  singly linked list is a palindrome.
+ *  @head: pointer to start of linked list
+ *  Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ *  Description: if head pointer is equal to null return 1
+ *  set flag to 1 and assigned two pointers right and left
+ *  to point to the start and end of the list and compare
+ *  intigers at those nodes if the intigers are equal the left
+ *  pointer is moved forward and the right is moved backward kind of
+ *  with the use of a marker mk.. just read the code...
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *lt = *head;
+	listint_t *rt = *head;
+	listint_t *mkr = NULL;
+	int flag = 1;
+
+	if (*head == NULL)
+		return (1);
+
+	while (flag == 1)
+	{
+		rt = *head;
+		while (rt->next != mkr)
+			rt = rt->next;
+		mkr = rt;
+
+		if (lt->n != rt->n)
+		{
+			flag = 0;
+			break;
+		}
+		if (lt->next == rt || lt == rt)
+			break;
+
+		lt = lt->next;
+	}
+	return (flag);
+}
