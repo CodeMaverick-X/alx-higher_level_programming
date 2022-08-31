c3dade90222841091f90ad1344af86823e8d23c1
reinhard <reinhard2057@gmail.com>
2022-08-31 12:44:20 +0000

    thus

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new = matrix.copy()
+    for i in range(0, len(new)):
+        for j in range(len(new[i])):
+            new[i][j] =  new[i][j]**2
+
+    return new
