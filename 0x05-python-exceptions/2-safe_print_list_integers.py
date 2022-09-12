fa8e231c3218363bbd81059cf89c172a43985892
reinhardcode <reinhard2057.com>
2022-09-12 12:42:39 +0000

    Revert "hot fix"
    This reverts commit c367477c78847a16b2c8268b787e645fc407b66f.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,23 +2,17 @@
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
     i = 0
+    try:
+        for n in range(x):
+            try:
+                print("{:d}".format(my_list[i]), end="")
+                count = count + 1
+            except (ValueError, TypeError):
+                pass
+            i = i + 1
 
-    for n in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count = count + 1
-        except (ValueError, TypeError):
-            pass
-        i = i + 1
-
-    print()
+        print()
+    except IndexError:
+        print()
 
     return (count)
-
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
