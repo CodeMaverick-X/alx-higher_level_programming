6acf9be0a08d42fc6fc05cbb7b8bdd757a7f0573
reinhardcode <reinhard2057.com>
2022-09-13 21:08:18 +0000

    test

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,10 +11,6 @@ class Square:
         to size that was pased in
         private instance attribute: size"""
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
