3fa9e96e2268edcfaa34f3b1c296db5f19c67198
reinhardcode <reinhard2057.com>
2022-09-13 20:05:18 +0000

    task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """square class based on 0-sqaure.py
+    Instantiation with size (no type/value verification).
+    private instance attribute: size"""
+
+    def __init__(self, size):
+        """initialises the size attr"""
+        self.__size = size
+
