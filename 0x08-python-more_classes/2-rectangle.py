283ff3d9dd54725d1bc36db02e5f71e19cbccc15
reinhardcode <reinhard2057.com>
2022-09-20 00:17:53 +0000

    late af

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,6 +49,6 @@ class Rectangle:
 
     def perimeter(self):
         """calcs the perimeter no test cases"""
-        if self.__weight == 0 or self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * self.__height + 2 * self.__weight
+        return 2 * self.__height + 2 * self.__width
