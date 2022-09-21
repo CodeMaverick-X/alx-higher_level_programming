27032e5677dee9d8d4c910fccc43c1cbe2da618b
reinhardcode <reinhard2057.com>
2022-09-21 22:12:46 +0000

    thyu

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 class LockedClass:
 
-    __slots__ = ['first_name']
+    __slots__ = ['first_name', 'f_name']
 
     def __init__(self, first_name=''):
-        self.first_name = first_name
+        self.f_name = first_name
+
+
+    @property
+    def first_name(self):
+        return self.f_name
+
+    @first_name.setter
+    def first_name(self, value):
+        self.f_name = value
+
+
