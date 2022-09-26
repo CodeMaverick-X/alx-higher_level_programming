800734057718a32f7a93400775caac6346b50eb5
reinhardcode <reinhard2057.com>
2022-09-26 19:15:29 +0000

    thisiì

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,6 +4,8 @@ module: 0-lookup contains a function called lookup
 that returns the list of available attr and methods
 of an object.
 """
+
+
 def lookup(obj):
     """
     arg: obj which is an object
