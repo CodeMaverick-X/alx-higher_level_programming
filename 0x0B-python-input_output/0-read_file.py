3d03374e83ed57aa28ffbf4a0ebb52812d540ce4
reinhardcode <reinhard2057.com>
2022-09-27 19:24:09 +0000

    this

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,5 +8,8 @@ def read_file(filename=""):
     """opens <filename> and prints to stdout"""
 
     with open(filename, encoding="utf-8") as fp:
-        data = fp.read()
-        print(data)
+        while True:
+            data = fp.readline()
+            if not data:
+                break
+            print(data)
