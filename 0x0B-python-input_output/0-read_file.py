5a890795bf952ca3aece115b417a085de357e9f4
reinhardcode <reinhard2057.com>
2022-09-27 19:07:51 +0000

    this

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+mod to read a text file and print to stdout
+"""
+
+
+def read_file(filename=""):
+    """opens <filename> and prints to stdout"""
+
+    with open(filename, encoding="utf-8") as fp:
+        print(fp.read())
