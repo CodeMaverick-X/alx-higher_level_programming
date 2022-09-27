7ae9c9a6be161da5560e1fa1770bdbaa4d8e4c1d
reinhardcode <reinhard2057.com>
2022-09-27 04:06:12 +0000

    tjis

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module for geometry
+"""
+
+
+class BaseGeometry:
+    """
+    has to do with some geometry
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
