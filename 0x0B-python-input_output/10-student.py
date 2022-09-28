3fa91feed2927708b0f62b4dc5a0a1f9475acd21
reinhardcode <reinhard2057.com>
2022-09-28 11:20:24 +0000

    test tee

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -25,9 +25,9 @@ class Student:
         """
         if (type(attrs) == list and
                 all(type(x) == str for x in attrs)):
-            new_dict = dict()
+            new = dict()
             for x in attrs:
                 if x in self.__dict__:
-                    new_dict.update({x: self.__dict__[x]})
-            return new_dict
+                    new.update({x: self.__dict__[x]})
+            return new
         return self.__dict__.copy()
