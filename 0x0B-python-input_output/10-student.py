3c7afe15e59ce31f995459517a3f2038936e4eb2
reinhardcode <reinhard2057.com>
2022-09-28 11:04:43 +0000

    thsiì

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""student module with class student"""
 
 
 class Student:
-    """Represent a student."""
+    """class student and blah blah"""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
-        """
+        """Description: instantiation"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student.
-        If attrs is a list of strings, represents only those attributes
-        included in the list.
-        Args:
-            attrs (list): (Optional) The attributes to represent.
         """
+        Discription: retrieves a dict representation
+        of student
+
+        Args:
+            - attrs: list of atrributes
+        Returns: dictionary
+        """
+
         if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
-            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+                all(type(x) == str for x in attrs)):
+            new = dict()
+            for l in attrs:
+                if l in self.__dict__:
+                    new[l] = self.__dict__[l]
+            return new
         return self.__dict__
