16e39bd166a075dc4bfa9771c23075ebcb78b202
reinhardcode <reinhard2057.com>
2022-10-02 17:19:00 +0000

    json sting to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,8 +22,24 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """convert list of dictionary to json str"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         else:
             e_data = json.dumps(list_dictionaries)
             return e_data
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the json string representation of list_objs to file"""
+        list_dict = []
+        for i in list_objs:
+            list_dict.append(i.to_dictionary())
+
+        e_data = Base.to_json_string(list_dict)
+
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding="utf-8") as fp:
+            fp.write(e_data)
+
+
