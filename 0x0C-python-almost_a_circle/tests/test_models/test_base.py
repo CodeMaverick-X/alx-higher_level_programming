8fcde098d4eed24b2f17afdaa8c56128683853f0
reinhardcode <reinhard2057.com>
2022-10-03 22:59:15 +0000

    this

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -30,3 +30,17 @@ class testbase(unittest.TestCase):
         """test instance"""
         b1 = Base()
         self.assertEqual(type(b1), Base)
+
+
+    def test_03(self):
+        """test to_json_string"""
+
+        d = {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+        json_d = Base.to_json_string([d])
+        self.assertTrue(isinstance(d, dict))
+        self.assertTrue(isinstance(json_d, str))
+        json_d_1 = Base.to_json_string([])
+        self.assertEqual(json_d_1, "[]")
+        json_d_2 = Base.to_json_string(None)
+        self.assertEqual(json_d_1, "[]")
+
