3790ffdb45a2d972c598215107ccc71caa09b7b3
reinhard <reinhard2057@gmail.com>
2022-11-28 23:37:57 +0100

    task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
 
-let i = 0;
-let j;
-let flag = 0;
-for (j in process.argv) {
-  if (i >= 2) {
-    console.log(process.argv[j]);
-    flag = 1;
-  }
-  i++;
-}
-
-if (flag === 0) {
+if (process.argv[2]) {
+  console.log(process.argv[2]);
+} else {
   console.log('No argument');
 }
