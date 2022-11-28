2c9077e4380989c78baeb4b7dc4e57d9aee1e40c
reinhard <reinhard2057@gmail.com>
2022-11-28 23:32:39 +0100

    task 12

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,12 +3,16 @@ const process = require('process');
 let max = 0;
 let sMax = 0;
 let i;
+let j = 0;
 
 for (i in process.argv) {
-  i = process.argv[i];
-  if (i >= max) {
-    sMax = max;
-    max = i;
+  if (j >= 2) {
+    i = process.argv[i];
+    if (i >= max) {
+      sMax = max;
+      max = i;
+    }
   }
+  j++;
 }
 console.log(sMax);
