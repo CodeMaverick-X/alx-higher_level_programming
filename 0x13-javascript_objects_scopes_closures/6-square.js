50ddd474aaf3d8603f719b225539a79e8c277a76
reinhard <reinhard2057@gmail.com>
2022-11-29 20:03:10 +0100

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      let i;
+      let j;
+      for (i = 0; i < this.height; i++) {
+        for (j = 0; j < this.width; j++) {
+          process.stdout.write(c);
+        }
+        console.log();
+      }
+    }
+  }
+}
+
+module.exports = Square;
