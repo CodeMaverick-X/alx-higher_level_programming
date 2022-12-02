d5ca2719dd177704fde83bd4b2cf4c67caede859
reinhard <reinhard2057@gmail.com>
2022-12-02 11:29:51 +0000

    task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""connect to mysql server and qet data from cites table"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    c = db.cursor()
+    sql_cmd = "SELECT * FROM cities"
+
+    c.execute(sql_cmd)
+    result = c.fetchall()
+
+    for i in result:
+        print(i)
+
+    c.close()
+    db.close()
