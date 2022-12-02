6320b4e836c539a437f1a6ad476db16cf5460f0d
reinhard <reinhard2057@gmail.com>
2022-12-02 10:26:34 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""connect to mysql server and qet data"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    c = db.cursor()
+
+    c.execute("SELECT * FROM states WHERE name = %s", (state_name,))
+    result = c.fetchall()
+
+    for i in result:
+        print(i)
+
+    c.close()
+    db.close()
