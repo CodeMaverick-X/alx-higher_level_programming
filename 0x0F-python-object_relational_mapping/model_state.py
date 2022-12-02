6940f3d21a11c7fdb97571128488e13d5db9d97a
reinhard <reinhard2057@gmail.com>
2022-12-02 15:37:59 +0000

    task 6

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    class state to represent state table.
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+"""url="mysql://reinhard:password@localhost:3306/hbtn_0e_0_usa"
+engine = create_engine(url, echo=True)
+Base.metadata.create_all(engine)"""
