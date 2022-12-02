b32e1a76648a086d147a211b1d4284a74ad9c53a
reinhard <reinhard2057@gmail.com>
2022-12-02 09:13:17 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,14 @@ username = argv[1]
 password = argv[2]
 database = argv[3]
 f = 0
-try:
-    db = MySQLdb.connect(user=username, passwd=password, db=database)
-    f = 1
-except Exception:
-    print("error acceccing db")
-if f == 1:
-    c = db.cursor()
 
-    c.execute("select * from states")
-    result = c.fetchall()
+db = MySQLdb.connect(host="localhost", port=3306,user=username, passwd=password, db=database)
 
-    for i in result:
-        print(i)
+
+c = db.cursor()
+
+c.execute("select * from states")
+result = c.fetchall()
+
+for i in result:
+    print(i)
