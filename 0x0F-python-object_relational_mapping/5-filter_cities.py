10f58f42996b73b649bafa60d6e9fc05c9c9c720
reinhard <reinhard2057@gmail.com>
2022-12-02 12:02:24 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""connect to mysql server and qet data from cities table"""
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_name = argv[4].strip("'\"")
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    c = db.cursor()
+    sql_cmd = "SELECT * FROM cities\
+            WHERE state_id = (SELECT id from states\
+            WHERE {} = %s)".format("name")
+    value = (state_name,)
+
+    c.execute(sql_cmd, value)
+    result = c.fetchall()
+
+    j = 0
+    for i in result:
+        print(i[2], end="")
+        if (j != (len(result) - 1)):
+            print(", ", end="")
+        j = j + 1
+
+    print()
+
+    c.close()
+    db.close()
