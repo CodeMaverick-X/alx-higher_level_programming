bef42fcdbe9e23f2cec92060be3a0814623d26bf
reinhard <reinhard2057@gmail.com>
2022-12-16 08:58:26 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL
-# and displays the size of the body of the response
 text=$(curl -s -I "$1" | grep Content-Length)
-IFS=' '
 read -r -a strarr <<< "$text"
 echo "${strarr[1]}"
