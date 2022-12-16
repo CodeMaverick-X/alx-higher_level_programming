9ee35685738d5cbfcde0b55321e477cf6e97e6f6
reinhard <reinhard2057@gmail.com>
2022-12-16 21:22:38 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,10 +5,11 @@ from urllib.request import urlopen, Request
 
 from urllib.request import urlopen, Request
 
-request = Request("https://alx-intranet.hbtn.io/status")
-with urlopen(request) as rsp_obj:
-    obj = rsp_obj.read()
-    obj_d = obj.decode("utf-8")
-    print("\t- type: {}".format(type(obj)))
-    print("\t- content: {}".format(obj))
-    print("\t- utf8 content: {}".format(obj_d))
+if __name__ = "__main__":
+    request = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(request) as rsp_obj:
+        obj = rsp_obj.read()
+        obj_d = obj.decode("utf-8")
+        print("\t- type: {}".format(type(obj)))
+        print("\t- content: {}".format(obj))
+        print("\t- utf8 content: {}".format(obj_d))
