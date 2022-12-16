cd0669a983944b1c295d9c906b82409610c4926f
reinhard <reinhard2057@gmail.com>
2022-12-16 21:32:53 +0000

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     with urlopen(request) as rsp_obj:
         obj = rsp_obj.read()
         obj_d = obj.decode("utf-8")
+
+        print("Body response:")
         print("\t- type: {}".format(type(obj)))
         print("\t- content: {}".format(obj))
         print("\t- utf8 content: {}".format(obj_d))
