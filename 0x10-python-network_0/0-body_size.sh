fc3ffe204b3dcbdd91a61de1e926e7120ccd8857
reinhard <reinhard2057@gmail.com>
2022-12-16 09:02:33 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a request to that URL
-text=$(curl -s -I "$1" | grep Content-Length)
-read -r -a strarr <<< "$text"
-echo "${strarr[1]}"
+curl -s "$1" | wc -c
