bbc2c3e385b1db6ed400b2c10ba3d4c3dfcdba7a
reinhard <reinhard2057@gmail.com>
2022-12-16 13:15:23 +0000

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays all the http methods a server will accept
-curl -isLX OPTIONS $1 | grep 'Allow:' | cut -d ':' -f2 | | sed 's/^ //'
+curl -isLX OPTIONS $1 | grep 'Allow:' | cut -d ':' -f2 | sed 's/^ //'
