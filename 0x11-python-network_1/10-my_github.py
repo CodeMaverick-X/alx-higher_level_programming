23adecb4b04a80b6c1d7c9857aaa769a7ff7c9ce
reinhard <reinhard2057@gmail.com>
2022-12-31 19:48:58 +0000

    task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+get user id from a github id using the gihub api
+using basic authentication
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
+
+    if r.status_code == 200:
+        print(r.json().get("id"))
